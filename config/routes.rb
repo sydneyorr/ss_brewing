2c67b734550ad54c3b5023db94c9c002ce584d05
Sydney Orr <sydneywhitters@gmail.com>
2021-10-06 10:10:58 -0600

    posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
+    get "/users", to: "users#index"
+    get "/users/:id", to: "users#show"
+    put "/users/:id", to: "users#update"
+
     resources :things
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
